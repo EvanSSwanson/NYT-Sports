f800946a051afd4b316a6367cd79904bbffcb7a2
Evan Swanson <evan.sundelius@gmail.com>
2023-02-22 18:07:01 -0600

    complete header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,5 @@
 import './Header.css';
-import React, { useEffect } from "react";
-import { Route, Routes, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from '../../assets/icons8-soccer-goal-100-red.png'
 
 function Header() {
@@ -10,7 +9,10 @@ function Header() {
             <img src={logo} className="logo" alt="logo" />
             <h1 className='title'>NYT Sports</h1>
         </NavLink>
-        <a className='source' href={'https://developer.nytimes.com/docs/top-stories-product/1/overview'} target="_blank">API Source</a>
+        <div className='header-right'>
+            <a className='source' href={'https://developer.nytimes.com/docs/top-stories-product/1/overview'} target="_blank">API Source</a>
+            <a className='github' href={'https://github.com/EvanSSwanson'} target="_blank">GitHub</a>
+        </div>
     </div>
   );
 }
