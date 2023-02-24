fe5121eb0635fc7bb3f122f2122053bd098e2a4f
Evan Swanson <evan.sundelius@gmail.com>
2023-02-23 18:38:11 -0600

    Fix search bug

--- src/Components/MainView/MainView.js
+++ src/Components/MainView/MainView.js
@@ -8,16 +8,19 @@ const MainView = (props) => {
     const [filtered, setFiltered] = useState(props.articles);
 
     const handleChange = (e) => {
-        e.preventDefault();
+        e.preventDefault()
         setSearchInput(e.target.value);
-        setFiltered(props.articles.filter((story) =>
-        story.title.toLowerCase().includes(searchInput)))
       };
 
     useEffect(() => {
         window.scrollTo(0, 0)
         setFiltered(props.articles)
       }, [])
+
+      useEffect(() => {
+        setFiltered(props.articles.filter((story) =>
+        story.title.toLowerCase().includes(searchInput)))
+      }, [searchInput])
 
     const filteredResponses = filtered.map((story) => {
         return (
@@ -60,26 +63,10 @@ const MainView = (props) => {
             type="text"
             placeholder="Search articles..."
             onChange={handleChange}
-
           />
         </form>
         <div className="all-card-container">
             {searchInput === '' ? fullResponses : filteredResponses}
-            {/* {filtered.map((story) => {
-        return (
-            <NavLink to='/article' onClick={() => props.setSelected(story)}>
-                <Card
-                    key={story.uri}
-                    id={story.uri}
-                    title={story.title}
-                    section={story.subsection}
-                    byline={story.byline}
-                    image={story.multimedia[1].url}
-                    imageAlt={story.multimedia[1].caption}
-                />
-            </NavLink>
-        )
-        })} */}
         </div>
     </div>
   );
